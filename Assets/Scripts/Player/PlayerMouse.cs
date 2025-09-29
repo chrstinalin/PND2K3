dee86d29530448e3298fbf1c345e6cbbdcd8d37e
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2025-09-28 20:45:58 -0400

    In-Game UI (#80)

--- Assets/Scripts/Player/PlayerMouse.cs
+++ Assets/Scripts/Player/PlayerMouse.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMouse : MonoBehaviour
 {
     private Health _health;
+    private Image[] HealthPoints;
     
     void Start()
     {
+        HealthPoints = GameObject.FindGameObjectWithTag("MouseHealthPointContainer").GetComponentsInChildren<Image>();
         _health = GetComponent<Health>();
+        _health.onHealthChanged.AddListener(OnHealthChanged);
         _health.onDeath.AddListener(OnDeath);
     }
     
+    public void OnHealthChanged(int damage)
+    {
+        for (int i = 0; i < _health.GetMaxHealth(); i++)
+        {
+            HealthPoints[i].enabled = i <= _health.GetCurrHealth() - 1;
+        }
+    }
+
     public void OnDeath()
     {
         Debug.Log("Player Died. Respawning...");
